beba2ae90899a5c59b090d6c0cd3a087c43de0d7
aavillalbab <arithalexis@gmail.com>
2019-12-27 21:57:11 -0500

    Settings in the main module configuration file

--- src/config/main.config.ts
+++ src/config/main.config.ts
@@ -1,17 +1,37 @@
 import { Configuration } from './configuration.interface';
 
 const version = 'v1';
+const name = 'sigasac';
 
+/** Archivo de configuración para el módulo main */
 export const MAIN: Configuration = {
-    name: 'sigasac',
+    name,
     version,
     description: 'Módulo Siga Sac',
     port: 3000,
     host: '127.0.0.1',
-    apiBasePath: `sigasac/${version}`,
+    apiBasePath: `${name}/${version}`,
     subRoutes: {
-        schools: 'schools',
+        accountsTypes: 'accounts-types',
+        banks: 'banks',
+        budgetAccounts: 'budget-accounts',
+        campus: 'campus',
+        countries: 'countries',
         login: 'login',
-        country: 'countries',
+        natureDocuments: 'nature-documents',
+        typesPeople: 'type-persons',
+        projects: 'projects',
+        regimes: 'regimes',
+        revenues: 'revenues',
+        revenuesTypes: 'revenues-types',
+        schools: 'schools',
+        schoolBankAccounts: 'school-bank-accounts',
+        singleAccountPlan: 'single-account-plan',
+        thirdParties: 'third-parties',
+        thirdPartyAccounts: 'third-party-accounts',
+        thirdPartyTypes: 'third-party-types',
+        typesAdministratorDocuments: 'types-administrator-documents',
+        typesSchoolDocuments: 'types-school-documents',
+        vouchers: 'vouchers'
     }
 };
