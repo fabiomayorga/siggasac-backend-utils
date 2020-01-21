c6617073342109f73766c813b20c43c07710f05b
aavillalbab <arithalexis@gmail.com>
2020-01-21 12:44:44 -0500

    file config users added

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,4 @@
 export { CONFIGURATION } from './configurations.config';
 export { MAIN } from './main.config';
 export { ROLES } from './roles';
+export { USERS } from './users.config';
---
+++ src/config/users.config.ts
@@ -0,0 +1,18 @@
+import { Configuration } from './configuration.interface';
+
+const version = 'v1';
+const name = 'users';
+
+/** Archivo de configuración para el módulo users */
+export const USERS: Configuration = {
+    name,
+    version,
+    description: 'Módulo de manejo de usuarios - Siga Sac',
+    port: 3001,
+    host: '127.0.0.1',
+    apiBasePath: `${name}/${version}`,
+    subRoutes: {
+        main: 'main',
+        recoverPassword: 'revocer-password'
+    }
+};
