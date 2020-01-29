4296a03f5f3c852e4fc18da18ab530f47b77b122
aavillalbab <arithalexis@gmail.com>
2020-01-29 09:06:33 -0500

    configuration file is added for the data module

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,5 @@
 export { CONFIGURATION } from './configurations.config';
+export { DATA } from './data.config';
 export { MAIN } from './main.config';
 export { ROLES } from './roles';
 export { USERS } from './users.config';
